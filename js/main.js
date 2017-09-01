8d25df08b62f0239354aac62be6a4d5aca0150f2
bobbyquennell <bobbyquennell@gmail.com>
2017-09-01 17:33:33 +1000

    85% done, need to fix the carousel stuff

--- js/main.js
+++ js/main.js
@@ -1,55 +1,66 @@
 // Avoid `console` errors in browsers that lack a console.
-(function(){for(var a,e=function(){},b="assert clear count debug dir dirxml error exception group groupCollapsed groupEnd info log markTimeline profile profileEnd table time timeEnd timeStamp trace warn".split(" "),c=b.length,d=window.console=window.console||{};c--;)a=b[c],d[a]||(d[a]=e)})();
+(function() {
+  for (var a, e = function() {},
+  b = "assert clear count debug dir dirxml error exception group groupCollapsed groupEnd info log markTimeline profile profileEnd table time timeEnd timeStamp trace warn".split(" "),
+  c = b.length,
+  d = window.console = window.console || {}; c--;)
+    a = b[c],
+    d[a] || (d[a] = e)
+})();
 
 // Site js object
 var Site;
 
-(function ($) {
-	'use strict'; 
-	
-	Site = {
-		/**
+(function($) {
+  'use strict';
+
+  Site = {
+    /**
 		 * Tabs for each product on tactile device
 		 *
 		 */
-		init: function (elt) {
+    init: function(elt) {
 
-			this.videoEmbedOnClick();			
-		},
-		/**
+      this.videoEmbedOnClick();
+      $('.carousel-item').slick({infinite: true, slidesToShow: 3, slidesToScroll: 3
+     });
+    },
+    /**
 		 * Switch video poster to embed youtube autoplay on click
 		 *
 		 */
-		videoEmbedOnClick: function(){
-			// click to show video
-			var videoIsPlaying = false;
+    videoEmbedOnClick: function() {
+      // click to show video
+      var videoIsPlaying = false;
 
-			$('.videoCTA').on('click', function(e){
-				e.stopPropagation();
+      $('.videoCTA').on('click', function(e) {
+        e.stopPropagation();
 
-				var $videoContainer = $(this).parent();
-				var urlToPlay = $videoContainer.data('src');
+        var $videoContainer = $(this).parent();
+        var urlToPlay = $videoContainer.data('src');
 
-				$videoContainer.find('.videoCTA').hide();
-				$videoContainer.find('.videoWrapper').html('<iframe width="560" height="315" src="'+urlToPlay+'" frameborder="0" allowfullscreen></iframe>').show();
-				videoIsPlaying = true;
-			});
+        $videoContainer.find('.videoCTA').hide();
+        $videoContainer.find('.video-wrapper').html('<iframe width="560" height="315" src="' + urlToPlay + '?autoplay=1&control=0showinfo=0" frameborder="0" allowfullscreen></iframe>').show();
+        videoIsPlaying = true;
+      });
 
-			// click outside of the video id needed
-			$('body').on('click', function(e){
-				e.stopPropagation();
+      // click outside of the video id needed
+      $('body').on('click', function(e) {
+        e.stopPropagation();
 
-				if(videoIsPlaying) {
-					$('.videoWrapper').html('');
-					$('.video-player').hide();
-					$('.videoCTA').show();
-					videoIsPlaying = false;
-				}
-			});
-		}
-	};
+        if (videoIsPlaying) {
+          $('.video-wrapper').html('');
+          $('.video-player').hide();
+          $('.videoCTA').show();
+          videoIsPlaying = false;
+        }
+      });
+    }
+  };
 
-	// call init function
-	window.onload = function(){ Site.init(); }
+  // call init function
+  window.onload = function() {
+    Site.init();
+  }
 
 })(jQuery);
